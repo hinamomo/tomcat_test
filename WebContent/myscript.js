f721e29098abf976abce1c3237bce008824fae30
Momoko Ishimura <applemelody03@gmail.com>
2019-10-12 16:54:49 +0900

    スプライトを関数に切り出し

--- WebContent/myscript.js
+++ WebContent/myscript.js
@@ -7,10 +7,10 @@ function preload() {
 
 function setup() {
   createCanvas(400, 400);
-  spr = createSprite( width/2, height/2, 40, 40);
-  //spr.shapeColor = color(255);
-  spr.velocity.y = 0.5;
-  spr.addImage(balloonImage);
+  createBalloon(width/2,height/2);
+  //spr = createSprite( width/2, height/2, 40, 40);
+  //spr.velocity.y = 0.5;
+  //spr.addImage(balloonImage);
 }
 
 function draw() {
@@ -21,4 +21,16 @@ function draw() {
 function mousePressed() {
   spr.position.x = mouseX;
   spr.position.y = mouseY;
+}
+
+//風船を作る
+function createBalloon(x, y) {
+    // 風船のスプライトを作る
+    var balloon = createSprite(x, y,40,40);
+
+    // 風船の画像を追加する
+    balloon.addImage(balloonImage);
+
+    // 風船を上に移動させるための速度を代入する
+    balloon.velocity.y = random(-1.5, -1);
 }
