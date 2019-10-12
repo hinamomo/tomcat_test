e2865f51b0fd4a436cbbf201a3ee531402014f2d
Momoko Ishimura <applemelody03@gmail.com>
2019-10-12 16:50:54 +0900

    スプライトに画像追加

--- WebContent/myscript.js
+++ WebContent/myscript.js
@@ -1,10 +1,16 @@
 let spr;
+let balloonImage;
+
+function preload() {
+    balloonImage = loadImage('balloon.png');
+}
 
 function setup() {
   createCanvas(400, 400);
   spr = createSprite( width/2, height/2, 40, 40);
-  spr.shapeColor = color(255);
+  //spr.shapeColor = color(255);
   spr.velocity.y = 0.5;
+  spr.addImage(balloonImage);
 }
 
 function draw() {
